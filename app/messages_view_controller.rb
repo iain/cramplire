a3efdad2e52524f2df53231f85076cb3bd00ae6e
iain <iain@iain.nl>
2012-06-29 15:20:13 +0200

    Put textfield at bottom and move it with the keyboard

--- app/messages_view_controller.rb
+++ app/messages_view_controller.rb
@@ -78,7 +78,7 @@ class MessagesViewController < UIViewController
   end
 
   def input
-    @input ||= UITextField.alloc.initWithFrame(CGRectMake(10, 10, view.size.width - 20, 31)).tap do |t|
+    @input ||= UITextField.alloc.initWithFrame([[10, input_top], [view.size.width - 20, 31]]).tap do |t|
       t.delegate        = self
       t.placeholder     = 'Type your message'
       t.borderStyle     = UITextBorderStyleRoundedRect
@@ -89,10 +89,71 @@ class MessagesViewController < UIViewController
   end
 
   def table
-    @table ||= UITableView.alloc.initWithFrame([[0, 50], [view.size.width, view.size.height - 50]], style: UITableViewStylePlain).tap do |t|
+    @table ||= UITableView.alloc.initWithFrame([[0, 0], [view.size.width, table_height]], style: UITableViewStylePlain).tap do |t|
       t.delegate    = self
       t.dataSource  = self
     end
+  end
+
+  def table_height
+    view.size.height - 137
+  end
+
+  def input_top
+    table_height + 10
+  end
+
+  KEYBOARD_ANIMATION_DURATION = 0.3
+  MINIMUM_SCROLL_FRACTION     = 0.2
+  MAXIMUM_SCROLL_FRACTION     = 0.8
+  PORTRAIT_KEYBOARD_HEIGHT    = 216
+  LANDSCAPE_KEYBOARD_HEIGHT   = 162
+
+  def textFieldDidBeginEditing(textField)
+    textFieldRect = view.window.convertRect(textField.bounds, fromView: textField)
+    viewRect = view.window.convertRect(view.bounds, fromView: view)
+
+    midline = textFieldRect.origin.y + 0.5 * textFieldRect.size.height
+    numerator = midline - viewRect.origin.y - MINIMUM_SCROLL_FRACTION * viewRect.size.height
+    denominator = (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION) * viewRect.size.height
+    heightFraction = numerator / denominator
+
+    if (heightFraction < 0.0)
+      heightFraction = 0.0
+    elsif (heightFraction > 1.0)
+      heightFraction = 1.0
+    end
+
+    orientation = UIApplication.sharedApplication.statusBarOrientation
+    if (orientation == UIInterfaceOrientationPortrait || orientation == UIInterfaceOrientationPortraitUpsideDown)
+      @animatedDistance = (PORTRAIT_KEYBOARD_HEIGHT * heightFraction).floor
+    else
+      @animatedDistance = (LANDSCAPE_KEYBOARD_HEIGHT * heightFraction).floor
+    end
+
+    viewFrame = self.view.frame
+    viewFrame.origin.y -= @animatedDistance
+
+    UIView.beginAnimations(nil, context:nil)
+    UIView.setAnimationBeginsFromCurrentState(true)
+    UIView.setAnimationDuration(KEYBOARD_ANIMATION_DURATION)
+
+    view.setFrame(viewFrame)
+
+    UIView.commitAnimations
+  end
+
+  def textFieldDidEndEditing(textField)
+    viewFrame = self.view.frame
+    viewFrame.origin.y += @animatedDistance
+
+    UIView.beginAnimations(nil, context:nil)
+    UIView.setAnimationBeginsFromCurrentState(true)
+    UIView.setAnimationDuration(KEYBOARD_ANIMATION_DURATION)
+
+    view.setFrame(viewFrame)
+
+    UIView.commitAnimations
   end
 
 end
